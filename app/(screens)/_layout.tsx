f9f724767dbfbad157015e60c1203db6473a90b6
Edward <edward.rl101@gmail.com>
2024-07-30 18:02:59 +0800

    Fixed sudden issues

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -100,6 +100,8 @@ export default function RootLayout() {
      <Drawer.Screen name="Friend" component={Friends} initialParams={{user: _user}}/>
      <Drawer.Screen name="Profile" component={Profile} initialParams={{user: _user}}/>
      <Drawer.Screen name="Statistics" component ={Statistics} initialParams={{user: _user}}/>
+     <Drawer.Screen name="Store" component ={Store} initialParams={{user: _user}}/>
+     <Drawer.Screen name="Settings" component ={Settings} initialParams={{user: _user}}/>
   </Drawer.Navigator>
   );
 }
